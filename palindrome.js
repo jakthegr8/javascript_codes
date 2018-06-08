0955fba011455ef469f8caae36a47c3d79c9d8e0
Arun Kumar <arun.ranush@gmail.com>
2018-06-08 17:43:50 +0530

    Palindrome and reverse integer

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+// Method 1
+function isPalindrome1 (str) {
+  return str === str.split('').reverse().join('')
+}
+
+// Method 2
+function isPalindrome2 (str) {
+  return str.split('').every((char, i) => {
+      return char === str[str.length - i - 1]
+  })
+}
+
+console.log(isPalindrome1('abba'));
+console.log(isPalindrome1('Arun'));
+
+console.log(isPalindrome2('abba'));
+console.log(isPalindrome2('Arun'));
---
+++ reverseInt.js
@@ -0,0 +1,7 @@
+function reverseInt(int) {
+  let revInt = parseInt(int.toString().split('').reverse().join(''), 10)
+  return revInt * Math.sign(int)
+}
+
+console.log(reverseInt(800));
+console.log(reverseInt(-590));
