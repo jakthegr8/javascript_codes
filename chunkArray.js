b5a9ce777d5c5072f2913a196d23a53d9e3bb839
Arun Kumar <arun.ranush@gmail.com>
2018-06-08 20:02:32 +0530

    Another way of chunk array

--- chunkArray.js
+++ chunkArray.js
@@ -1,3 +1,4 @@
+// Method 1
 function chunkArray(array, size) {
   const chunked = [];
   for (let ele of array) {
@@ -11,4 +12,15 @@ function chunkArray(array, size) {
   return chunked;
 }
 
+// Method 2
+function chunkArraySlice(array,  size) {
+  let offset = 0, chunked = [];
+  while (offset <= array.length) {
+    chunked.push(array.slice(offset, offset = offset + size));
+  }
+  return chunked;
+}
+
+
 console.log(chunkArray([1,2,3,4,5,6,7], 2));
+console.log(chunkArraySlice([1,2,3,4,5,6,7], 2));
