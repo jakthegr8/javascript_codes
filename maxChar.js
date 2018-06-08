fcdf4a28400d61091e09c3ae16e0c5050d1dcae3
Arun Kumar <arun.ranush@gmail.com>
2018-06-08 18:05:45 +0530

    Find max char

---
+++ maxChar.js
@@ -0,0 +1,13 @@
+function findMaxChar(str) {
+  let charMap = {}, maxCount = 0, maxChar = '';
+  for (let char of str.split('')) {
+    charMap[char] = charMap[char] + 1 || 1;
+    if (maxCount < charMap[char]) {
+      maxChar = char
+      maxCount = charMap[char]
+    }
+  }
+  return maxChar;
+}
+
+console.log(findMaxChar('sooooper'));
