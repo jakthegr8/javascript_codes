ffb03e55c98eab0fef8b3ee30f551c34e621178f
Arun Kumar <arun.ranush@gmail.com>
2018-06-09 14:53:30 +0530

    Speed up fibonacci with memoization logic

---
+++ fibonacci_memoization.js
@@ -0,0 +1,20 @@
+function slowFib(n) {
+  if (n < 2) { return n; }
+  return fib(n-1) + fib(n-2);
+}
+
+function memoiz(fn) {
+  let cache = {}
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    } else {
+      let result = fn.apply(this, args);
+      cache[args] = result;
+      return result;
+    }
+  }
+}
+
+const fib = memoiz(slowFib)
+console.log(fib(100));
