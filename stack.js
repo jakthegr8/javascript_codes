ca7b2b72d567ae40a2fc33d873c00c3888a8568e
Arun Kumar <arun.ranush@gmail.com>
2018-06-09 16:11:03 +0530

    Stack data structure

---
+++ stack.js
@@ -0,0 +1,15 @@
+class Stack {
+  constructor() { this.data = []; }
+  add(ele) { this.data.push(ele); }
+  remove() { this.data.pop(); }
+  print() { console.log(this.data);}
+}
+
+let stack = new Stack();
+
+stack.add(1);
+stack.add(2);
+stack.add(3);
+stack.print();
+stack.remove()
+stack.print();
