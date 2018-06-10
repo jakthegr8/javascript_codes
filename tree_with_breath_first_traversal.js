a43d57dd1226999a786b9114e330216b166bba2a
Arun Kumar <arun.ranush@gmail.com>
2018-06-10 15:08:10 +0530

    Tree with breath first traversal

---
+++ tree_with_breath_first_traversal.js
@@ -0,0 +1,41 @@
+class Node {
+  constructor(data) {
+    this.data = data
+    this.children = [];
+  }
+
+  add(value) {
+    this.children.push(new Node(value))
+  }
+
+  remove(value) {
+    this.children = this.children.filter(ele => { return ele.data !== value });
+  }
+}
+
+class Tree {
+  constructor(root=null) {
+    this.root = root
+  }
+
+  traverseBF() {
+    let arr = [this.root];
+    console.log(arr);
+    while (arr.length) {
+      let firstNode = arr.shift();
+      console.log(firstNode.data);
+      arr.push(...firstNode.children)
+    }
+  }
+}
+
+let node = new Node(1);
+node.add(2);
+node.add(3);
+node.children[0].add(4)
+node.children[0].add(5)
+node.children[1].add(6)
+node.children[1].add(7)
+
+let tree = new Tree(node);
+tree.traverseBF()
