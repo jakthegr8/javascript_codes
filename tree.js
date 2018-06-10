cd525c88a5b28d76114b351ae4dbf38252dab860
Arun Kumar <arun.ranush@gmail.com>
2018-06-10 15:01:35 +0530

    Tree implementation with couple of child nodes

---
+++ tree.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(data) {
+    this.data = data
+    this.children = [];
+  }
+
+  add(value) {
+    this.children.push(new Node(value))
+  }
+
+  remove(value) {
+    this.children = this.children.filter(ele => { return ele.data !== value });
+  }
+}
+
+class Tree {
+  constructor(root=null) {
+    this.root = root
+  }
+
+}
+
+let node = new Node(1);
+node.add(2);
+node.add(3);
+node.children[0].add(4)
+node.children[0].add(5)
+node.children[1].add(6)
+node.children[1].add(7)
+
+let tree = new Tree(node);
+console.log(tree);
